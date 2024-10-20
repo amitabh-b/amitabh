9fcc059f92cb8b81047357d9486281fb2fe271fc
amitabh-b <amitabh.baruah@harness.io>
2024-10-20 11:20:08 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div>
+      <h1>Hi, I am Amitabh 👋 </h1>
+      <p className="read-the-docs">
+       I love building web-apps
+      </p>
+    </div>
+  )
+}
+
+export default App
