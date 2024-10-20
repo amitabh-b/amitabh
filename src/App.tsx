154c47c4dd186ab36766cded0006804c8793df6c
amitabh-b <amitabh.baruah@harness.io>
2024-10-20 22:47:44 +0530

    updated content

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
         <h1>Amitabh Baruah.</h1>
       </div>
       <p className="read-the-docs">
-        I passionate about building innovative web applications. I am a Software
+        I passionate about building innovative web applications and I am a Software
         Engineer currently working at <a className="link" href="https://www.harness.io/" target="__blank">Harness</a>.
       </p>
     </div>
